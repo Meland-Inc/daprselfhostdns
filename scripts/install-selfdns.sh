b87e8a04c45126c60e57c35faba51a443a60a02d
AlbertHuang <r.albert.huang@gmail.com>
2022-10-17 16:03:17 +0800

    chore: Support pubsub

--- scripts/install-selfdns.sh
+++ scripts/install-selfdns.sh
@@ -11,16 +11,19 @@ while [ -h "$SOURCE" ]; do
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 readonly PROJECT_ROOT="$(dirname $DIR)"
+readonly consulAddress=${ip:-"192.168.50.171"}
 RUN_ROOT="$PROJECT_ROOT"
 cd $PROJECT_ROOT;
 
-consulAddress="192.168.50.171:8500"
+fullConsulAddress="$consulAddress:8500"
 # echo "请输入dns地址";
 # read -r consulAddress
 # consulAddress="$consulAddress"
 
-echo "当前使用dns url: ${consulAddress}";
+echo "当前使用dns url: ${fullConsulAddress}";
  
-cat "$PROJECT_ROOT/config/consul.yaml" | sed "s/\$consulAddress/$consulAddress/g" > ~/.dapr/components/consul.yaml;
+cat "$PROJECT_ROOT/config/consul.yaml" | sed "s/\$consulAddress/$fullConsulAddress/g" > ~/.dapr/components/consul.yaml;
 
-cat "$PROJECT_ROOT/config/config.yaml" | sed "s/\$consulAddress/$consulAddress/g" > ~/.dapr/config.yaml;
+cat "$PROJECT_ROOT/config/config.yaml" | sed "s/\$consulAddress/$fullConsulAddress/g" > ~/.dapr/config.yaml;
+
+cat "$PROJECT_ROOT/config/pubsub.yaml" | sed "s/\$localhost/$consulAddress/g" > ~/.dapr/components/pubsub.yaml;
